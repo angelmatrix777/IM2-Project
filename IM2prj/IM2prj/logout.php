6e4b48a44d9677f2676bf451725fa13334408495
Angel Transcending <143064140+angelmatrix777@users.noreply.github.com>
2024-07-17 11:53:39 +0800

    Delete IM2prj directory

--- IM2prj/IM2prj/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header("Location: index.html"); // Redirect to login page or home page
-exit();
-?>
--- IM2prj/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header("Location: login.php"); // Redirect to login page or home page
-exit();
-?>
